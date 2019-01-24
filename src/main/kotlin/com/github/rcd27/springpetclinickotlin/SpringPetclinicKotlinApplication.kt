9688e289ce02e13a384efb8f1bc1ea8a62683233
Zemlyakov Stas <redtom@yandex.ru>
2019-01-24 18:09:12 +0300

    Initial commit

---
+++ src/main/kotlin/com/github/rcd27/springpetclinickotlin/SpringPetclinicKotlinApplication.kt
@@ -0,0 +1,12 @@
+package com.github.rcd27.springpetclinickotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringPetclinicKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringPetclinicKotlinApplication>(*args)
+}
+
